50790f47b0a33d6bfe29ae4f586920b96d5ae2fb
huytd072 <huytd072@gmail.com>
2024-06-26 23:37:41 +0700

    add read and count word

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_word=count_word(text)
+    #print(text)
+    print(num_word)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+def count_word(text):
+    words=[]
+    words=text.split()
+    count=0
+    for word in words:
+        count+=1
+    return count
+
+
+
+main()
+    
+
